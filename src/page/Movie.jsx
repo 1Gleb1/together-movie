d0cb9d7111c344a280300d1e6de8a3828d47608b
1Gleb1 <glen2004111@gmail.com>
2022-03-10 16:03:40 +0300

    movieStyle02

--- src/page/Movie.jsx
+++ src/page/Movie.jsx
@@ -70,7 +70,7 @@ const Movie = () => {
                 {movie.imdb_id}
             </div> */}
 
-            <div className='m-32 flex justify-center bg-gray-700  w-full h-[250px] sm:h-[520px]'>{/* max-w-7xl w-full h-[520px] */}
+            <div className=' m-12 flex justify-center bg-gray-700  w-full h-[250px] sm:h-[520px]'>{/* max-w-7xl w-full h-[520px] */}
                 <div className="relative w-full max-w-[340px] sm:max-w-[720px]"> 
                     <iframe 
                         src={`https://74.svetacdn.in/DRQQUUcW0qvr?imdb_id=${movie.imdb_id}`}//imdb_id=${movie.imdb_id}
